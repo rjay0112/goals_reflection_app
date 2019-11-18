2ac2a95ae6a24868d1eab354a338a1cb03755579
Ryan Jay <ryan.jay@ontariotechu.net>
2019-11-17 23:52:37 -0500

    initial push

---
+++ lib/MainPage.dart
@@ -0,0 +1,139 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color selectedColor = Colors.black;
+  Color pickerColor = Colors.black;
+  double strokeWidth = 3.0;
+  List<DrawingPoints> points = List();
+  bool showBottomList = false;
+  double opacity = 1.0;
+  SelectedMode selectedMode = SelectedMode.StrokeWidth;
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+    Colors.black
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*appBar: AppBar(
+        title: Text(widget.title),
+      ),*/
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: <Widget>[
+            _goals(),
+            _drawingArea(),
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), 
+    );
+  }
+
+  Widget _goals(){
+    return Container(
+      height: MediaQuery.of(context).size.height*0.4,
+      color: Colors.green,
+
+    );
+  }
+
+  Widget _drawingArea(){
+    return GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  //..strokeCap = strokeCap
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanStart: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  //..strokeCap = strokeCap
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanEnd: (details) {
+          setState(() {
+            points.add(null);
+          });
+        },
+        child: ClipRect(
+          child:CustomPaint(
+          size: Size(MediaQuery.of(context).size.width,MediaQuery.of(context).size.height*0.2),//Size.infinite,
+          child: Container(
+            //color: Colors.orange,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height*0.5,
+          ),
+          painter: DrawingPainter(
+            pointsList: points,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter({this.pointsList});
+  List<DrawingPoints> pointsList;
+  List<Offset> offsetPoints = List();
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      if (pointsList[i] != null && pointsList[i + 1] != null) {
+        canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
+            pointsList[i].paint);
+      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
+        offsetPoints.clear();
+        offsetPoints.add(pointsList[i].points);
+        offsetPoints.add(Offset(
+            pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
+        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
+
+class DrawingPoints {
+  Paint paint;
+  Offset points;
+  DrawingPoints({this.points, this.paint});
+}
+
+enum SelectedMode { StrokeWidth, Opacity, Color }
